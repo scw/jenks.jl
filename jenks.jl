20f4484f64fc92ea5eb2dfe6e8341ad509dc6cb0
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-03-26 14:51:14 -0400

    Create a type container for the data used instead of passing around the same two variables to each function. Automate type conversion.

--- jenks.jl
+++ jenks.jl
@@ -1,73 +1,96 @@
 #!/usr/bin/env julia
 
-function jenks_matrix_init(data, n_classes)
-  n_data = length(data)
-  lower_class_limits  = zeros(n_data + 1, n_classes + 1)
-  lower_class_limits[2,2:n_classes+1] = 1.0
+module Jenks
 
-  variance_combinations = zeros(n_data + 1, n_classes + 1)
-  variance_combinations[3:n_data+1, 2:n_classes+1] = Inf 
-  return lower_class_limits, variance_combinations
-end
+  type DataClassification
+    data::Array{Float64,1}
+    n_classes::Int
 
-function jenks_matrices(data, n_classes)
-  @inbounds begin
-    lower_class_limits, variance_combinations = jenks_matrix_init(data, n_classes)
-   
-    for l in 2:length(data)
-      sum = 0.0
-      sum_squares = 0.0
-      w = 0.0
-      variance = 0.0
-      for m in 1:l
-        # 'III' originally
-        lcl = l - m + 1
-        val = data[lcl]
+    function DataClassification(data::Array{Any,1}, n_classes::Int)
+      new(convert(Array{Float64,1}, data), n_classes)
+    end
 
-        w += 1
-        sum += val
-        sum_squares += val*val
+    function DataClassification(data::Array{Int,1}, n_classes::Int)
+      new(convert(Array{Float64,1}, data), n_classes)
+    end
 
-        variance = sum_squares - (sum*sum) / w
-        if lcl != 1
-          for j in 2:n_classes
-            var_plus_prev = variance + variance_combinations[lcl, j]
-            if variance_combinations[l+1, j+1] >= var_plus_prev
-              lower_class_limits[l+1, j+1] = lcl
-              variance_combinations[l+1, j+1] = var_plus_prev
+    function DataClassification(data::Array{Float64,1}, n_classes::Int)
+      new(data, n_classes)
+    end
+  end
+
+
+  function jenks_matrix_init(dc::DataClassification)
+    n_data = length(dc.data)
+    dims = (n_data + 1, dc.n_classes + 1)
+
+    lower_class_limits  = zeros(dims)
+    lower_class_limits[2,2:dc.n_classes+1] = 1.0
+
+    variance_combinations = zeros(dims)
+    variance_combinations[3:n_data+1, 2:dc.n_classes+1] = Inf 
+
+    return lower_class_limits, variance_combinations
+  end
+
+  function jenks_matrices(dc::DataClassification)
+    @inbounds begin
+      lower_class_limits, variance_combinations = jenks_matrix_init(dc)
+    
+      for l in 2:length(dc.data)
+        sum = 0.0
+        sum_squares = 0.0
+        w = 0.0
+        variance = 0.0
+        for m in 1:l
+          # 'III' originally
+          lcl = l - m + 1
+          val = dc.data[lcl]
+
+          w += 1
+          sum += val
+          sum_squares += val*val
+
+          variance = sum_squares - (sum*sum) / w
+          if lcl != 1
+            for j in 2:dc.n_classes
+              var_plus_prev = variance + variance_combinations[lcl, j]
+              if variance_combinations[l+1, j+1] >= var_plus_prev
+                lower_class_limits[l+1, j+1] = lcl
+                variance_combinations[l+1, j+1] = var_plus_prev
+              end
             end
           end
         end
+        lower_class_limits[l+1,2] = 1.0
+        variance_combinations[l+1,2] = variance
       end
-      lower_class_limits[l+1,2] = 1.0
-      variance_combinations[l+1,2] = variance
     end
-  end
-  return lower_class_limits, variance_combinations
-end
-
-function jenks(data, n_classes)
-  if n_classes > length(data)
-    return
+    return lower_class_limits, variance_combinations
   end
 
-  sort!(data)
+  function jenks(dc::DataClassification)
+    if dc.n_classes > length(dc.data)
+      return
+    end
 
-  lower_class_limits, variance_combinations = jenks_matrices(data, n_classes)
+    sort!(dc.data)
 
-  k = length(data)
-  class = zeros(n_classes+1)
-  count_num = n_classes
+    lower_class_limits, variance_combinations = jenks_matrices(dc)
 
-  class[n_classes+1] = data[k]
-  class[1] = data[1]
+    k = length(dc.data)
+    count_num = dc.n_classes
 
-  while count_num > 1
-    elt = int(lower_class_limits[k,count_num+1] - 1)
-    class[count_num] = data[elt]
-    k = int(lower_class_limits[k,count_num+1])
-    count_num -= 1
+    classification = zeros(dc.n_classes+1)
+    classification[dc.n_classes+1] = dc.data[k]
+    classification[1] = dc.data[1]
+
+    for count_num=dc.n_classes:-1:2
+      elt = int(lower_class_limits[k,count_num+1] - 1)
+      classification[count_num] = dc.data[elt]
+      k = int(lower_class_limits[k,count_num+1])
+    end
+
+    return classification
   end
-
-  return class
 end
